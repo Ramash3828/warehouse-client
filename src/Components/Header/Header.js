3e51090f4334ce1871af3c20ce284e6be7b37bba
Ramash3828 <ramash3828@gmail.com>
2022-04-29 02:09:08 +0600

    navbar done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,78 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
 import "./Header.css";
 
 const Header = () => {
     return (
         <div>
-            <h2>Header Page</h2>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">
+                        BiCycle Store
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarText"
+                        aria-controls="navbarText"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    to="/"
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    to="/manageitems"
+                                >
+                                    Manage Items
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    to="/additem"
+                                >
+                                    Add Item
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    to="/myitems"
+                                >
+                                    My Items
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link active"
+                                    aria-current="page"
+                                    to="/login"
+                                >
+                                    Login
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
--- src/Components/NootFound/NotFound.js
+++ src/Components/NootFound/NotFound.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Page Not Found</h2>
+        <div
+            className="d-flex flex-column justify-content-center align-items-center bg-gray"
+            style={{ height: `80vh` }}
+        >
+            <h1 className="d-block" style={{ fontSize: "100px" }}>
+                404
+            </h1>
+            <h1 style={{ fontSize: `50wh` }}>Page Not Found!!!</h1>
         </div>
     );
 };
