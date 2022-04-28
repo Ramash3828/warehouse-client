aa19049231fa3f6e293c2752690d476f78eb5022
Ramash3828 <ramash3828@gmail.com>
2022-04-29 00:42:41 +0600

    routing ready without menubar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import ManageItems from "./Components/ManageItems/ManageItems";
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/SignUp/SignUp";
+import AddItem from "./Components/AddItem/AddItem";
+import NotFound from "./Components/NootFound/NotFound";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
     return (
         <div className="App">
-            <h2>Welcome my Project</h2>
+            <Header></Header>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+                <Route
+                    path="/manageitems"
+                    element={<ManageItems></ManageItems>}
+                ></Route>
+                <Route path="/login" element={<Login></Login>}></Route>
+                <Route path="/signup" element={<SignUp></SignUp>}></Route>
+                <Route path="/additem" element={<AddItem></AddItem>}></Route>
+                <Route path="*" element={<NotFound></NotFound>}></Route>
+            </Routes>
+            <Footer></Footer>
         </div>
     );
 }
