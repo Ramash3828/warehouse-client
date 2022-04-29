f81df8fff65d8bdd6321cbd14e292c5c89b3d5bc
Ramash3828 <ramash3828@gmail.com>
2022-04-30 02:55:42 +0600

    security the routing for private route

--- src/requireAuth.js
+++ src/requireAuth.js
@@ -9,18 +9,21 @@ function RequireAuth({ children }) {
     if (loading) {
         return <Loading></Loading>;
     }
-    if (!user.emailVerified) {
-        console.log("Please Verify your Mail");
-        return;
-    }
+
     if (!user) {
-        // Redirect them to the /login page, but save the current location they were
-        // trying to go to when they were redirected. This allows us to send them
-        // along to that page after they login, which is a nicer user experience
-        // than dropping them off on the home page.
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
-
+    if (!user.emailVerified) {
+        return (
+            <div className="my-5 py-5">
+                <h2 className="text-danger">
+                    Please Verify your Email Address!!
+                </h2>
+                <h5>Check your email account and</h5>
+                <h5>verify your email then Refresh the webpage.</h5>
+            </div>
+        );
+    }
     return children;
 }
 export default RequireAuth;
