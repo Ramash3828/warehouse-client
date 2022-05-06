7517e67b4af78006473d9df04c86a21bb86653e3
Ramash3828 <ramash3828@gmail.com>
2022-05-06 20:12:06 +0600

    dealership reqruting

--- src/Components/GetJoin/GetJioin.js
+++ src/Components/GetJoin/GetJioin.js
@@ -3,13 +3,9 @@ import React from "react";
 const GetJioin = () => {
     return (
         <div style={{ backgroundColor: `#253858` }} className="py-5 text-white">
-            <p>
-                <small>
-                    IT SERVICE MANAGEMENT THAT UNLOCKS HIGH-VELOCITY TEAMS
-                </small>
-            </p>
+            <p>Verification and selection is going on for dealership</p>
             <h2>Try BiCycle Store Service Management now</h2>
-            <button className="update-btn bg-warning m-3">Get it free</button>
+            <button className="update-btn bg-warning m-3">Get it</button>
         </div>
     );
 };
